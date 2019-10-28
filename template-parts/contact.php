c06c28c0159f62f5a27fdec7b6345e229b614eb2
Nancy <ng.toledo@hotmail.com>
2019-10-27 23:17:33 -0600

    Template-parts

---
+++ template-parts/contact.php
@@ -0,0 +1,20 @@
+<section id="contacto">
+    <h1>Contacto</h1>
+    <div class="microsite-separator-bot"></div>
+    <form action="">
+        <p>Si deseas contactarnos o enviar tus recetas para compartirlas, escríbenos</p>
+        <div class="form-group">
+            <label for="name">Nombre</label>
+            <input type="text" name="name" value="Escribe tu nombre">
+        </div>
+        <div class="form-group">
+            <label for="name">E-mail</label>
+            <input type="text" name="email" value="Escribe tu correo">
+        </div>
+        <div class="form-group">
+            <label for="name">Mensaje</label>
+            <textarea name="mensaje"></textarea>
+        </div>
+        <button class="btn">Enviar Mensaje</button>
+    </form>
+</section>
