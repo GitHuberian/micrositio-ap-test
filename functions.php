6dd4168d9ba21f07e1868d4e0f81b0ef35714602
Nancy <ng.toledo@hotmail.com>
2019-10-27 23:08:34 -0600

    Menu movil final y configuraciones extras

--- functions.php
+++ functions.php
@@ -4,34 +4,46 @@ function microsite_scripts_styles() {
     //carga de la hoja principal de estilos
     wp_enqueue_style('style', get_stylesheet_uri(), array(), '1.0.0');
 }
+add_action('wp_enqueue_scripts', 'microsite_scripts_styles');
 
-    //longitud del excerpt/subtitulo a 50 caracteres
+    //longitud del excerpt/subtitulo a 50 caracteres y remate con vínculo al post
 function get_excerpt(){
-    $excerpt = get_the_content();
-    $excerpt = preg_replace(" ([.*?])",'',$excerpt);
-    $excerpt = strip_shortcodes($excerpt);
-    $excerpt = strip_tags($excerpt);
+    $excerpt = get_the_excerpt();
     $excerpt = substr($excerpt, 0, 50);
-    $excerpt = substr($excerpt, 0, strripos($excerpt, " "));
     $excerpt = trim(preg_replace( '/\s+/', ' ', $excerpt));
     $excerpt = $excerpt.'... <a href="'.get_the_permalink().'">| Ver más</a>';
     return $excerpt;
 }
 
-add_action('wp_enqueue_scripts', 'microsite_scripts_styles');
+if ( !function_exists( 'microsite_setup' ) ) {
+    function microsite_setup() {
+            //soporte tag title del sitio
+        add_theme_support( 'title-tag' );
 
-    //agregando la imagen destacada para un post
-add_theme_support('post-thumbnails');
+            //agregando la imagen destacada para un post
+        add_theme_support('post-thumbnails');
 
-    //soporte para personalizar el logotipo del sitio
-add_theme_support( 'custom-logo' );
+            //soporte para personalizar el logotipo del sitio
+        add_theme_support( 'custom-logo' );
 
-    //soporte para imagen de fondo del tema
-add_theme_support( 'custom-background' );
+            //soporte para imagen de fondo del tema
+        add_theme_support( 'custom-background' );
 
-    //soporte para imagen del heroe/header
-add_theme_support( 'custom-header' );
+            //soporte para imagen del heroe/header
+        add_theme_support( 'custom-header' );
+    }
 
+    add_action( 'after_setup_theme', 'microsite_setup' );
+}
+ 
+function microsite_title( $title ){
+    //cambiar el título (aparecerá en la pestaña del navegador) del sitio dependiendo de la página
+  if ( empty( $title ) && ( is_home() || is_front_page() ) ) {
+    $title = __( 'Home', 'textdomain' ) . ' | ' . get_bloginfo( 'description' );
+  }
+  return $title;
+}
+add_filter( 'wp_title', 'microsite_title' );
 
 function microsite_menus(){
     //registrar el menú principal del tema
--- index.php
+++ index.php
@@ -1,55 +1,60 @@
 <?php include 'header.php'; ?>
 <header>
-        <?php 
-            $custom_logo_id = get_theme_mod( 'custom_logo' );
-            $logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
-            if ( has_custom_logo() ) {
-                    echo '<img src="' . esc_url( $logo ) . '" alt="' . get_bloginfo( 'name' ) . '">';
-            } else {
-                    echo '<h1 class="microsite-title">'. get_bloginfo( 'name' ) .'</h1>';
-            }
-        ?>
-        <nav class="microsite-nav">
-            <div class="microsite-separator-top"></div>
-            <?php wp_nav_menu(array(
-                'theme_location' => 'menu-principal',
-                'container' => 'div'
-            )); ?>
-            <div class="microsite-separator-bot"></div>
-        </nav>
-        <div class="microsite">
-            <div class="microsite_featured-img">
-                <div class="microsite_featured-img_filter"></div>
-                <img src="<?php header_image(); ?>" />
-            </div>
-            <h1 class="microsite__title">El sabor de un momento para compartir</h1>
-            <p class="microsite__description">Disfruta de las recetas que hacemos con amor y pasión</p>
+    <?php
+    $custom_logo_id = get_theme_mod('custom_logo');
+    $logo = wp_get_attachment_image_src($custom_logo_id, 'full');
+    if (has_custom_logo()) {
+        echo '<img src="' . esc_url($logo) . '" alt="' . get_bloginfo('name') . '">';
+    } else {
+        echo '<h1 class="microsite-title">' . get_bloginfo('name') . '</h1>';
+    }
+    ?>
+    <nav class="microsite-nav">
+        <div class="microsite-separator-top"></div>
+        <a href="javascript:void(0);" class="icon" onclick="mobile_menu()">
+            <!--<img src="<?php //echo get_template_directory_uri(); ?>/assets/bars-solid.svg" width="30" height="30" alt="menu">  -->
+            MENU
+        </a>
+        <?php wp_nav_menu(array(
+            'theme_location' => 'menu-principal',
+            'container' => 'div',
+            'menu_id' => 'microsite-menu'
+        )); ?>
+        <div class="microsite-separator-bot"></div>
+    </nav>
+    <div class="microsite">
+        <div class="microsite_featured-img">
+            <div class="microsite_featured-img_filter"></div>
+            <img src="<?php header_image(); ?>" />
         </div>
+        <h1 class="microsite__title">El sabor de un momento para compartir</h1>
+        <p class="microsite__description">Disfruta de las recetas que hacemos con amor y pasión</p>
+    </div>
 </header>
 <main id="site-content">
     <h1>Blog Posts</h1>
     <div class="microsite-separator-bot"></div>
 
     <div class="microsite-posts-grid">
-        <?php while(have_posts()): the_post(); ?>
-        <article class="microsite-posts-grid_post">
-            <div class="microsite-posts-grid_post_thumbnail">
-                <?php if ( has_post_thumbnail() ) : ?>
-                    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-                        <div class="microsite-posts-grid_post_thumbnail_filter"></div>
-                        <?php the_post_thumbnail(); ?>
-                    </a>
-                <?php endif; ?>
-            </div>
-            <div class="microsite-posts-grid_post_data">
-                <p class="date"><?php the_date(); ?></p>
-                <p class="author"><span>Por</span> <?php the_author(); ?></p>
-            </div>
-            <div class="microsite-posts-grid_post_content">
-                <h3><?php the_title(); ?></h3>
-                <p><?php echo get_excerpt(); ?></p>
-            </div>
-        </article>
+        <?php while (have_posts()) : the_post(); ?>
+            <article class="microsite-posts-grid_post">
+                <div class="microsite-posts-grid_post_thumbnail">
+                    <?php if (has_post_thumbnail()) : ?>
+                        <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                            <div class="microsite-posts-grid_post_thumbnail_filter"></div>
+                            <?php the_post_thumbnail(); ?>
+                        </a>
+                    <?php endif; ?>
+                </div>
+                <div class="microsite-posts-grid_post_data">
+                    <p class="date"><?php echo get_the_date(); ?></p>
+                    <p class="author"><span>Por</span> <?php the_author(); ?></p>
+                </div>
+                <div class="microsite-posts-grid_post_content">
+                    <h3><?php the_title(); ?></h3>
+                    <p><?php echo get_excerpt(); ?></p>
+                </div>
+            </article>
         <?php endwhile; ?>
     </div>
 </main>
